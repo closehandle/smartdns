c8e3c2f2c11dd8b73796f5ee1247015201386491
CloseHandle <88403145+closehandle@users.noreply.github.com>
2024-08-19 21:13:04 +0800

    优化配置文件

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,25 @@
 FROM debian:latest
 
 ADD smartdns.sh /usr/bin/smartdns.sh
-RUN chmod +x /usr/bin/smartdns.sh && \
-    chown root:root /usr/bin/smartdns.sh && \
-    apt update && \
+RUN apt update && \
     apt install curl -y && \
+    apt clean && \
+    chmod +x /usr/bin/smartdns.sh && \
+    chown root:root /usr/bin/smartdns.sh && \
     smartdns.sh
 
-FROM alpine:latest
-
-RUN apk --no-cache add ca-certificates
+FROM debian:latest
+COPY --from=0 /chinadns.list /etc/smartdns/chinadns.list
+COPY --from=0 /otherdns.list /etc/smartdns/otherdns.list
 
 ADD https://github.com/pymumu/smartdns/releases/download/Release46/smartdns-x86_64 /usr/bin/smartdns
 ADD smartdns.conf /etc/smartdns/smartdns.conf
-RUN chmod +x /usr/bin/smartdns && \
+RUN apt update && \
+    apt install ca-certificates -y && \
+    apt clean && \
+    chmod +x /usr/bin/smartdns && \
     chown root:root /usr/bin/smartdns && \
     mkdir /var/lib/smartdns
-
-COPY --from=0 /chinadns.list /etc/smartdns/chinadns.list
-COPY --from=0 /otherdns.list /etc/smartdns/otherdns.list
 
 ENTRYPOINT ["/usr/bin/smartdns"]
 CMD ["-f", "/etc/smartdns/smartdns.conf", "-p", "-"]
--- smartdns.sh
+++ smartdns.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-rm -f chinadns.list otherdns.list
-
 chinadns=$(curl --retry-all-errors \
     https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf \
     https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/google.china.conf \
@@ -8,15 +6,12 @@ chinadns=$(curl --retry-all-errors \
 otherdns=$(curl --retry-all-errors \
     https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt)
 
-chinadns=$(echo "${chinadns}" | sed 's|server=/||' | sed 's|/114.114.114.114||' | grep -Ev '^#')
-otherdns=$(echo "${otherdns}" | base64 -d | grep -vE '^\!|\[|^@@|(https?://){0,1}[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | sed -r 's#^(\|\|?)?(https?://)?##g' | sed -r 's#/.*$|%2F.*$##g' | grep -E '([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)' | sed -r 's#^(([a-zA-Z0-9]*\*[-a-zA-Z0-9]*)?(\.))?([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)(\*[a-zA-Z0-9]*)?#\4#g')
-
-chinadns=$(echo "${chinadns}" | sort | uniq | sort)
+chinadns=$(echo "${chinadns}" | sed 's|server=/||' | sed 's|/114.114.114.114||' | grep -Ev '^#' | sort | uniq | sort)
 while read -r i; do
     echo "${i}" >> chinadns.list
 done <<< "${chinadns}"
 
-otherdns=$(echo "${otherdns}" | sort | uniq | sort)
+otherdns=$(echo "${otherdns}" | base64 -d | grep -vE '^\!|\[|^@@|(https?://){0,1}[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | sed -r 's#^(\|\|?)?(https?://)?##g' | sed -r 's#/.*$|%2F.*$##g' | grep -E '([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)' | sed -r 's#^(([a-zA-Z0-9]*\*[-a-zA-Z0-9]*)?(\.))?([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)(\*[a-zA-Z0-9]*)?#\4#g' | sort | uniq | sort)
 while read -r i; do
     echo "${i}" >> otherdns.list
 done <<< "${otherdns}"
