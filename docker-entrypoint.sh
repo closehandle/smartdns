32a8f3e18eaca2f4d0f13c53992e2bd94d1cede5
CloseHandle <88403145+closehandle@users.noreply.github.com>
2025-05-26 12:21:54 +0800

    修复启动问题

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ -f /run/smartdns/smartdns.conf ]]; then
-    exec smartdns -f /run/smartdns/smartdns.conf -p -
+    exec smartdns -c /run/smartdns/smartdns.conf -f -p -
 else
-    exec smartdns -f /etc/smartdns/smartdns.conf -p -
+    exec smartdns -c /etc/smartdns/smartdns.conf -f -p -
 fi
