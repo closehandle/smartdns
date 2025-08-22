499f591dc78993ad03896acd4a26eb44185d547a
CloseHandle <88403145+closehandle@users.noreply.github.com>
2025-08-22 23:50:07 +0800

    正确处理下游域名列表

--- smartdns-rules.sh
+++ smartdns-rules.sh
@@ -7,7 +7,7 @@ chinadns=$(curl --retry-all-errors \
 otherdns=$(curl --retry-all-errors \
     https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt)
 
-echo "${chinadns}" | sed 's|server=/||' | sed 's|/114.114.114.114||' | grep -Ev '^#' | grep -E '.' | sort | uniq | sort > chinadns.list
+echo "${chinadns}" | sed 's|full:||g' | sed 's|server=/||g' | sed 's|/114.114.114.114||g' | grep -Ev '^regexp:' | grep -Ev '^#' | grep -E '.' | sort | uniq | sort > chinadns.list
 echo "${otherdns}" | base64 -d | grep -Ev '^\!|\[|^@@|(https?://){0,1}[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | sed -r 's#^(\|\|?)?(https?://)?##g' | sed -r 's#/.*$|%2F.*$##g' | grep -E '([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)' | sed -r 's#^(([a-zA-Z0-9]*\*[-a-zA-Z0-9]*)?(\.))?([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)(\*[a-zA-Z0-9]*)?#\4#g' | grep -Ev '^#' | grep -E '.' | sort | uniq | sort > otherdns.list
 
 sed -i '/^[[:space:]]*$/d' *.list
