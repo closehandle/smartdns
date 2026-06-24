dbac894731a767cfde3534900fc9fe4232a955c7
CloseHandle <88403145+closehandle@users.noreply.github.com>
2026-06-24 19:58:46 +0800

    不再集成 dnsproxy 客户端

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
-for ((i=0; ; i++)); do
-    name="DNS${i}"
-    if [[ -z "${!name}" ]]; then
-        break
-    fi
-
-    dnsproxy ${!name} &
-done
-
 if [[ -f /run/smartdns/smartdns.conf ]]; then
-    smartdns -c /run/smartdns/smartdns.conf -f -p - &
+    exec smartdns -c /run/smartdns/smartdns.conf -f -p -
 else
-    smartdns -c /etc/smartdns/smartdns.conf -f -p - &
+    exec smartdns -c /etc/smartdns/smartdns.conf -f -p -
 fi
-
-wait -n
-exit $?
